0b32d6c650f6526109bd1da02a01448afa345001
AllEyEsOnMe9977 <57782244+AllEyEsOnMe9977@users.noreply.github.com>
2025-08-01 21:05:11 +0330

    Update ready_install.sh

--- ready_install.sh
+++ ready_install.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
-set -euo pipefail
-
 cd /opt
-if [[ -d tgwebhooksetup ]]; then
-  echo "Removing old /opt/tgwebhooksetup ..."
-  sudo rm -rf tgwebhooksetup
-fi
-
-echo "Cloning tgwebhooksetup repository..."
 git clone https://github.com/AllEyEsOnMe9977/tgwebhooksetup.git
-
 cd tgwebhooksetup
-echo "Running main install.sh as root..."
-sudo bash install.sh
+bash ready_install.sh
